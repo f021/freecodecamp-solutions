e166b1b0d22abded861b0d55d33e8195808d6053
andrei <andrei.fzto@gmail.com>
2015-12-15 14:42:52 +0300

    sum-fib

--- intermediate-algorithm-scripting/14-sum-all-odd-fib-nums.js
+++ intermediate-algorithm-scripting/14-sum-all-odd-fib-nums.js
@@ -1,9 +1,15 @@
-function unite(arr1, arr2, arr3) {
-  return [].slice.call(arguments, 0).reduce(function(ps, ns) {
-    return ps.concat(ns.filter(function(n){
-      return ps.indexOf(n) === -1;    
-    }));
-  }, []);
+function sumFibs(num) {
+  var sum = 0;
+  function fib(n) {
+    if (n === 0 || n === 1) return 1;
+    return fib(n - 1) + fib(n - 2);
+  }
+  for(var i = 0; fib(i) <= num; i++) {
+    if (fib(i) % 2 === 1) {
+      sum += fib(i);
+    }
+  }
+  return sum;
 }
 
-unite([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+sumFibs(1000);
