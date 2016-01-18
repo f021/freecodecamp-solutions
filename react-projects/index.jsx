47b9a4055cc76d0ef44139bd96a117978c867283
andrei <andrei.fzto@gmail.com>
2016-01-18 10:49:26 +0300

    final

--- react-projects/index.jsx
+++ react-projects/index.jsx
@@ -1,83 +1,81 @@
-const mySort = ({ a, b, filter}) => {
-  if (typeof a[filter] === 'number') {
-    return a[filter] - b[filter];
-  }
-  return a[filter] > b[filter] ? 1 : -1;
-}
+const mySort = ({ a, b, key}) => 
+  (typeof a[key] === 'number')
+    ? a[key] - b[key]
+    : (a[key] > b[key]) ? 1 : -1;
 
 const reverse = (flag, arr) =>
   flag ? arr.reverse() : arr;
 
-const TRow = ({ index, img, username, recent, alltime }) => (
+const Header = () => (
   <tr>
-    <td>{index}</td>
-    <td>
-      <a className='avatar' href={'http://www.freecodecamp.com/' + username} target='_blank'>
-        <img src={img} />
-        <span>{username}</span>
+    <th colSpan='5' style={{backgroundColor: 'green'}}>
+      <a style={{backgroundColor: 'green'}} href='http://www.freecodecamp.com/'>
+        <img className ='img-responsive' src='https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg' />
       </a>
-    </td>
-    <td>{recent}</td>
-    <td>{alltime}</td>
+    </th>
   </tr>
 );
 
-
-const FilterLink = ({filterBy, children, filter, onClick, reverse}) => {
-  return (
-    (filterBy === filter)
-      ? <a href='#' onClick={onClick}>{children}{' '}{reverse ? '▼' : '▲'}</a>
-      : <span onClick={() => onClick(filter)}>{children}</span>
+const FilterLink = ({filterBy, children, filter, onClick, reverse}) => (
+  (filterBy === filter)
+    ? <a href='#' onClick={onClick}>{children}{' '}{reverse ? '▼' : '▲'}</a>
+    : <span style={{cursor: 'pointer'}} onClick={() => onClick(filter)}>{children}</span>
 );
-}
 
-const THead = (props) => {
-  return (
+const THead = (props) => (
   <thead>
+    <Header />
     <tr>
-      <th colSpan='4'>
-        <a href='http://www.freecodecamp.com/'>
-          <img src='https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg' />
-        </a>
+      <th className='col-md-1 text-center'>#</th>
+      <th className='col-md-5'>
+        <FilterLink filter='username' {...props}>users</FilterLink>
       </th>
-    </tr>
-    <tr>
-      <th>#</th>
-      <th><FilterLink filter='username' {...props}>users</FilterLink></th>
-      <th><FilterLink filter='recent' {...props}>last 30 days</FilterLink></th>
-      <th><FilterLink filter='alltime' {...props}>All time</FilterLink></th>
+      <th className='col-md-3 text-center'>
+        <FilterLink filter='recent' {...props}>last 30 days</FilterLink>
+      </th>
+      <th className='col-md-3 text-center'>
+        <FilterLink filter='alltime' {...props}>all time</FilterLink>
+      </th>
     </tr>
   </thead>
 );
-                         }
 
-const TBody = ({ users }) => {
-  return (
+const TRow = ({ index, img, username, recent, alltime }) => (
+  <tr>
+    <td className='text-center text-muted'>{index}</td>
+    <td>
+      <a className='avatar' href={'http://www.freecodecamp.com/' + username} target='_blank'>
+        <img className='img-circle' style={{height: '30px'}} src={img} />
+        <span style={{paddingLeft: '2em'}}>{username}</span>
+      </a>
+    </td>
+    <td className='text-center'>{recent}</td>
+    <td className='text-center'>{alltime}</td>
+  </tr>
+);
+
+const TBody = ({ users }) => (
   <tbody>
-    {users.map((user, index) =>
-      <TRow {...user} index={index + 1} />)}
+    {users.map( (user, index) =>
+      <TRow {...user} index={index + 1} /> )}
   </tbody>
 );
-                             }
 
 class Table extends React.Component {
   
   state = { filterBy: 'recent', reverse: true };
   
-  onClick = (filterBy) => {
+  onClick = (filterBy) =>
     (typeof filterBy === 'string')
       ? this.setState({ filterBy: filterBy, reverse: true })
-      : this.setState({ reverse: !this.state.reverse })
-  }
+      : this.setState({ reverse: !this.state.reverse });
   
   render() {
-    console.log(this.props.users)
     return (
-      <table>
+      <table className='table table-hover table-responsive'>
         <THead onClick={this.onClick} {...this.state} />
         <TBody users={reverse(this.state.reverse, this.props.users.sort((a,b) =>
-          mySort({a, b, filter: this.state.filterBy})))}
-        />
+          mySort({a, b, key: this.state.filterBy})))}/>
       </table>
     );
   }
@@ -85,15 +83,9 @@ class Table extends React.Component {
 
 class App extends React.Component {
   
-  static defaultProps = {
-    url: `http://fcctop100.herokuapp.com/api/fccusers/top/recent`
-  }
-    
-  static propTypes = {
-    url: React.PropTypes.string.isRequired
-  }
-  
-  state = { users: [] }
+  static defaultProps = {url: `http://fcctop100.herokuapp.com/api/fccusers/top/recent`};
+   
+  state = { users: [] };
   
   componentDidMount() {
     fetch(this.props.url, { mode: 'cors' })
@@ -103,7 +95,7 @@ class App extends React.Component {
   
   render () {
     return (
-      <Table {...this.state} />
+        <Table {...this.state} />
     );
   } 
 }
